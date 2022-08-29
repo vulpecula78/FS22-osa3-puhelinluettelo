6d3082e4d7ab878107934105da6bec180ae3c94e
Tino Pönni <tino.ponni@tutanota.com>
2022-08-29 19:33:05 +0300

    Tehtävät 3.13 - 3.14

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Entry = require('./models/person')
 const app = express()
 
 app.use(express.json())
@@ -13,47 +15,25 @@ app.use(express.static('build'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-	{
-		id: 1,
-		name: "Arto Hellas",
-		number: "040-123456"
-	},
-	{
-		id: 2,
-		name: "Ada Lovelace",
-		number: "39-44-5323523"
-	},
-	{
-		id: 3,
-		name: "Dan Abromov",
-		number: "12-43-234345"
-	},
-	{
-		id: 4,
-		name: "Mary Poppendick",
-		number: "39-23-6423122"
-	}
-]
-
 app.get('/info', (req, res) => {
 	const date = new Date
 	res.send (`<p>Phonebook has info for ${persons.length} people.</p><p>${date}</p>`)
 })
 
 app.get('/api/persons', (req, res) => {
-	res.json(persons)
+	Entry.find({}).then(persons => {
+		res.json(persons)
+	})
 })
 
 app.get('/api/persons/:id', (req, res) => {
-	const id = Number(req.params.id)
-	const person = persons.find(person => person.id === id)
-	
-	if (person) {	
-		res.json(person)
-	} else {
-		return res.status(404).end()
-	}
+	Entry.findById(req.params.id).then(person => {
+		if (person) {
+			res.json(person)
+		} else {
+			return res.status(404).end()
+		}
+	})
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -72,33 +52,28 @@ app.post('/api/persons', (req, res) => {
 		)
 	}
 
-	if (persons.find(person => person.name === body.name)) {
-		return res.status(400).json(
-			{ error: 'name must be unique' }
-		)
-	}
-
 	if (!body.number) {
 		return res.status(400).json(
 			{ error: 'number missing' }
 		)
 	}
 
-	const person = {
+	const person = new Entry({
 		id: generateId(),
 		name: body.name,
 		number: body.number
-	}
+	})
 
-	persons = persons.concat(person)
-	res.json(person)
+	person.save().then(savedPerson => {
+		res.json(savedPerson)
+	})
 })
 
 const generateId = () => {
 	return Math.floor(Math.random() * 50000)
 }
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
