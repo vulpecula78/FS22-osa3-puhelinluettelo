58b1517ed4ae3824671e46f61d07ecf2846c956f
Tino Pönni <tino.ponni@tutanota.com>
2022-09-19 21:44:48 +0300

    Tehtävät 3.16 - 3.18

--- index.js
+++ index.js
@@ -5,28 +5,32 @@ const cors = require('cors')
 const Entry = require('./models/person')
 const app = express()
 
+app.use(express.static('build'))
 app.use(express.json())
 
 morgan.token('body', function getBody (req) {
 	return JSON.stringify(req.body)
 })
 
-app.use(express.static('build'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.get('/info', (req, res) => {
+app.get('/info', (req, res, next) => {
 	const date = new Date
-	res.send (`<p>Phonebook has info for ${persons.length} people.</p><p>${date}</p>`)
+	Entry.countDocuments({}).then(count => {
+	  res.send (`<p>Phonebook has info for ${count} people.</p><p>${date}</p>`)
+	})
+	.catch(Error => next(error))
 })
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res, next) => {
 	Entry.find({}).then(persons => {
 	  res.json(persons)
 	})
+	.catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
 	Entry.findById(req.params.id).then(person => {
 	  if (person) {
 		res.json(person)
@@ -34,17 +38,15 @@ app.get('/api/persons/:id', (req, res) => {
 		return res.status(404).end()
 	  }
 	})
+	.catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
 	Entry.findByIdAndRemove(req.params.id)
 	  .then(result => {
 		res.status(204).end()
 	  })
-	  .catch(error => {
-		console.log(error)
-		res.status(400).send({error: 'invalid id'})
-	  })
+	  .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
@@ -74,9 +76,32 @@ app.post('/api/persons', (req, res) => {
 	})
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+	const body = req.body
+	const newData = {
+	  name: body.name,
+	  number: body.number
+	}
+	Entry.findByIdAndUpdate(req.params.id, newData, {new: true})
+	  .then(updated => {
+		res.json(updated)
+	  })
+	  .catch(error => next(error))
+})
+
 const generateId = () => {
 	return Math.floor(Math.random() * 50000)
 }
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+	return res.status(400).send({error: 'Invalid id!'})
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
