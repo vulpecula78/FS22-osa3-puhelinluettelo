63423687bbb3facf9c196c0d007edf64afc3e034
Tino Pönni <tino.ponni@tutanota.com>
2022-09-27 22:55:22 +0300

    Tehtävä 3.22

--- index.js
+++ index.js
@@ -9,97 +9,95 @@ app.use(express.static('build'))
 app.use(express.json())
 
 morgan.token('body', function getBody (req) {
-	return JSON.stringify(req.body)
+  return JSON.stringify(req.body)
 })
 
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/info', (req, res, next) => {
-	const date = new Date
-	Entry.countDocuments({}).then(count => {
-	  res.send (`<p>Phonebook has info for ${count} people.</p><p>${date}</p>`)
-	})
-	.catch(Error => next(error))
+  const date = new Date
+  Entry.countDocuments({}).then(count => {
+    res.send (`<p>Phonebook has info for ${count} people.</p><p>${date}</p>`)
+  })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons', (req, res, next) => {
-	Entry.find({}).then(persons => {
-	  res.json(persons)
-	})
-	.catch(error => next(error))
+  Entry.find({}).then(persons => {
+    res.json(persons)
+  })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-	Entry.findById(req.params.id).then(person => {
-	  if (person) {
-		res.json(person)
-	  } else {
-		return res.status(404).end()
-	  }
-	})
-	.catch(error => next(error))
+  Entry.findById(req.params.id).then(person => {
+    if (person) {
+      res.json(person)
+    } else {
+      return res.status(404).end()
+    }
+  })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-	Entry.findByIdAndRemove(req.params.id)
-	  .then(result => {
-		res.status(204).end()
-	  })
-	  .catch(error => next(error))
+  Entry.findByIdAndRemove(req.params.id)
+    .then(res.status(204).end())
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
-	const body = req.body
-	console.log(body)
+  const body = req.body
+  console.log(body)
 
-	if (!body.name) {
-	  return res.status(400).json(
-		{ error: 'name missing' }
-	  )
-	}
+  if (!body.name) {
+    return res.status(400).json(
+      { error: 'name missing' }
+    )
+  }
 
-	if (!body.number) {
-	  return res.status(400).json(
-		{ error: 'number missing' }
-	  )
-	}
+  if (!body.number) {
+    return res.status(400).json(
+      { error: 'number missing' }
+    )
+  }
 
-	const person = new Entry({
-	  id: generateId(),
-	  name: body.name,
-	  number: body.number
-	})
+  const person = new Entry({
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  })
 
-	person.save().then(savedPerson => {
-	  res.json(savedPerson)
-	})
-	  .catch(error => next(error))
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-	const body = req.body
-	const newData = {
-	  name: body.name,
-	  number: body.number
-	}
-	Entry.findByIdAndUpdate(req.params.id, newData, {new: true})
-	  .then(updated => {
-		res.json(updated)
-	  })
-	  .catch(error => next(error))
+  const body = req.body
+  const newData = {
+    name: body.name,
+    number: body.number
+  }
+  Entry.findByIdAndUpdate(req.params.id, newData, { new: true })
+    .then(updated => {
+      res.json(updated)
+    })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
-	return Math.floor(Math.random() * 50000)
+  return Math.floor(Math.random() * 50000)
 }
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
   if (error.name === 'CastError') {
-	return response.status(400).send({error: 'Invalid id!'})
+    return response.status(400).send({ error: 'Invalid id!' })
   } else if (error.name === 'ValidationError') {
-	  return response.status(400).json({error: error.message})
+    return response.status(400).json({ error: error.message })
   }
   next(error)
 }
@@ -108,5 +106,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -4,9 +4,7 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
+  .then(console.log('connected to MongoDB'))
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
@@ -20,7 +18,7 @@ const personSchema = new mongoose.Schema({
     type: String,
     validate: {
       validator: function(v) {
-        return /\d{3}-\d{5}|\d{2}-\d{6}/.test(v);
+        return /\d{3}-\d{5}|\d{2}-\d{6}/.test(v)
       },
       message: props => `${props.value} is not a valid phone number!`
     },
